0f634b11273e3cba066dfff601ac3fef14a01af7
dsrod <dsrod@seznam.cz>
2022-08-28 20:49:18 +0200

    fix: tabs vs spaces

--- MapRuler/index.ts
+++ MapRuler/index.ts
@@ -14,9 +14,9 @@ function init() {
     $("#map-ctx-buttons")
 	.append('<a id="mp_distance" class="mp" style="opacity: 0; left:226.5px; top:200px; background-position: -384px 0px;" href="/game.php?screen=map" data-title="Měřit vzdálenost od této vesnice"></a>')
 	.on('click', (e) => {
-		e.preventDefault()
+        e.preventDefault()
         window.TWMap.popup._cache = {}
-		window.TWMap.urls.villagePopup = window.TWMap.urls.villagePopup.replace(/source=([0-9])+/, `source=${window.TWMap.villages[window.TWMap.context._curFocus].id}`)
+        window.TWMap.urls.villagePopup = window.TWMap.urls.villagePopup.replace(/source=([0-9])+/, `source=${window.TWMap.villages[window.TWMap.context._curFocus].id}`)
         window.TWMap.context.hide()
 	})
 	.attr('title', "Měřit vzdálenost od této vesnice")
